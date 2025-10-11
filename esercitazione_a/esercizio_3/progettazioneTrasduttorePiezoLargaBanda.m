a0b39ab954eb84e5e307eb7423a5ba02145525f7
Michael Pio <michaelpio.stolfi@studenti.unibas.it>
2025-10-11 17:01:33 +0200

    αrefactor(esercitazione_a): migliorati commenti script esercizio 3

--- esercitazione_a/esercizio_3/progettazioneTrasduttorePiezoLargaBanda.m
+++ esercitazione_a/esercizio_3/progettazioneTrasduttorePiezoLargaBanda.m
@@ -76,20 +76,24 @@ k_plate = omega ./ v_plate;
 % A (3×3): modello elettromeccanico del solo piezo
 % [F1, F2, V]^T = A[v1, v2, I]^T
 % 
-% B (2×2): si ottiene dalla relazione precedente ricavando v1 dalla prima riga e sostituendolo nelle altre due 
+% B (2×2): piezo "ridotto" dopo aver fissato il lato sinistro, si ottiene 
+%          dalla relazione precedente ricavando v1 dalla prima riga e 
+%          sostituendolo nelle altre due 
 % [F2, V]^T = B[v2, I]^T
 % 
-% M (2×2): modello puramente meccanico di una piastra/layer interposto tra piezo e carico ZL, si ottiene dalla 
-%          prima relazione cancellando tutti i termini con h33(fattore di accoppiamento elettro-meccanico)
+% M (2×2): modello puramente meccanico di una piastra/layer interposto tra 
+%          piezo e carico ZL, si ottiene dalla prima relazione cancellando 
+%          tutti i termini con h33(fattore di accoppiamento elettro-meccanico)
 % [F3, F4]^T = M[v3, v4]^T
 M11 = ZoP ./ ( 1i .* tan(k_plate .* l_plate) );
 M12 = ZoP ./ ( 1i .* sin(k_plate .* l_plate) );
 M21 = M12;
 M22 = M11;
 
+% ???Dimostrare questa formula???
 Z = M11 - ( (M12 .^ 2) ./ (Z2 + M11) );
 
-% TODO: Capire se è corretto passarci B secondo me dovrei passarci M
+% ???TODO: Capire se è corretto passarci B secondo me dovrei passarci M???(NON CREDO PIÙ)
 [Zin_without_backing_with_plate, ~, ~]   = calcolaFunzioniDiTrasferimento(B, Z, Zel);
 [Zin_with_backing_with_plate, ~, ~] = calcolaFunzioniDiTrasferimento(B_B, Z, Zel);
 
