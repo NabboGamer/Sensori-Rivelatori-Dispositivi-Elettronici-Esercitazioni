b19e0e86c15e900f4755ebda48fba2dade7f28e7
Michael Pio <michaelpio.stolfi@studenti.unibas.it>
2025-10-20 21:04:21 +0200

    αfeat(esercitazione_a/cli): implementata stampa dei grafici per la versione finale del trasduttore a larga banda

--- esercitazione_a/cli/esercizio_3/progettazioneTrasduttorePiezoLargaBanda.m
+++ esercitazione_a/cli/esercizio_3/progettazioneTrasduttorePiezoLargaBanda.m
@@ -14,7 +14,7 @@ z_piezo = rho * v; % N.B.: Usare PZ27 per questa applicazione
 z_load = 1.5e+06;  % Acqua
 z2 = z_load;
 
-%% Funzione di trasferimento in trasmissione e in ricezione della ceramica con backing
+%% Funzione di trasferimento in trasmissione della ceramica con backing
 % Calcolo l'impedenza acustica della ceramica
 Zel = 1e+06;
 Z1 = areaFaccia*z1;
@@ -47,7 +47,7 @@ stampaGrafici(f, FTT_with_backing{1}, FTT_with_backing{2}, "Comparing TTF withou
 % hold on;
 % stampaGrafici(f, FTR_with_backing{1}, FTR_with_backing{2}, "Comparing RTF without and with Backing", 'orange', "RTF", "RTF", " with backing");
 
-%% Funzione di trasferimento in trasmissione e in ricezione della ceramica con backing e matching plate
+%% Funzione di trasferimento in trasmissione della ceramica con backing e matching plate
 
 % Calcolo l'impedenza acustica specifica della del piatto
 % (utilizzando la formula per massimizzare la banda passante)
@@ -104,7 +104,7 @@ FTT_without_backing_with_plate = {moduloFTT_without_backing_with_plate, faseFTT_
 FTT_with_backing_with_plate = {moduloFTT_with_backing_with_plate, faseFTT_with_backing_with_plate};
 
 l_plate_scaled = l_plate * 1e+03;
-l_plate_scaled = round(l_plate_scaled, 1);
+l_plate_scaled = round(l_plate_scaled, 2);
 legendTitle = strcat("Matching Plate made of E-Solder 3022 (", string(l_plate_scaled), " [mm])");
 figure(4)
 stampaGrafici(f, Zin_without_backing_with_plate{1}, Zin_without_backing_with_plate{2}, "Comparing Zin without and with Backing adding the matching plate", 'blue', "Zin", "Zin", " without backing with plate", legendTitle);
@@ -227,7 +227,6 @@ cprintf('Text',"\n");
 
 % Sezione utile per il disegno sul grafico delle bande passanti.
 % Commentato poichè rendeva il grafico colmo di linee e incomprensibile.
-
 % fig = gcf;                          % ultimo figure attivo
 % axs = findall(fig, 'Type', 'axes'); % tutti gli axes
 % ax1 = axs(end);                     % il "primo" subplot creato è l'ultimo della lista
@@ -278,12 +277,11 @@ for i = 1 : length(l_plate_values)
 
     M = calcolaMatriceM(ZoP, k_plate, l_plate);
     Zeq = M{1,1} - ( (M{1,2} .^ 2) ./ (Z2 + M{1,1}) );
-    [Zin_with_backing_with_plate, FTT_with_backing_pzt, ~] = calcolaFunzioniDiTrasferimento(B_with_backing, Zeq, Zel);
+    [~, FTT_with_backing_pzt, ~] = calcolaFunzioniDiTrasferimento(B_with_backing, Zeq, Zel);
     FTT_with_backing_pzt = FTT_with_backing_pzt{1} .* exp(1j*deg2rad(FTT_with_backing_pzt{2}));
     FTT_plate = ( M{1,2} .* Z2 ) ./ ( M{1,1}.*Z2 + M{1,1}.^2 - M{1,2}.^2);
     FTT_with_backing_with_plate = FTT_with_backing_pzt .* FTT_plate;
-    [moduloFTT_with_backing_with_plate, faseFTT_with_backing_with_plate] = calcolaModuloEFase(FTT_with_backing_with_plate, true, true);
-    FTT_with_backing_with_plate = {moduloFTT_with_backing_with_plate, faseFTT_with_backing_with_plate};
+    [moduloFTT_with_backing_with_plate, ~] = calcolaModuloEFase(FTT_with_backing_with_plate, true, true);
 
     A_max_with_backing_with_plate = max(moduloFTT_with_backing_with_plate);
     A_3dB_with_backing_with_plate = A_max_with_backing_with_plate - 3;
@@ -305,79 +303,43 @@ end
 cprintf('Text',"\n");
 cprintf('Comments', "Lo spessore ottimale è: l_plate=%0.4f\n", l_plate_best);
 cprintf('Comments', "La Banda Frazionaria a -3dB ottimale è: FBW=%0.2f%%\n", FBW_max);
-cprintf('Text',"\n");
-cprintf('Comments', "Spessore convertito: %0.4f → %0.4f\n", l_plate_best_previous, l_plate_best);
+cprintf('Comments', "Spessore convertito: %0.4f → %0.4f", l_plate_best_previous, l_plate_best);
 
-% l_plate = best_l_plate;
-% 
-% M11 = (ZoP./(1i.*tan((omega./v_plate).*l_plate)));
-% M12 = (ZoP./(1i.*sin((omega./v_plate).*l_plate)));
-% 
-% Z = M11-((M12.^2)./(Z2.*(1+(M11./Z2))));
-% 
-% [Zin_b, ~, ~, ~] = CalculateFunctions(B_b, Z, Zel, 0);
-% [Zin, ~, ~, ~] = CalculateFunctions(B, Z, Zel, 0);
-% 
-% figure(5)
-% Grafico(f, Zin{1}, Zin{2},'Z_i_n Backing with l-correction', 'blue');
-% hold on;
-% Grafico(f, Zin_b{1}, Zin_b{2},'Z_i_n Backing with l-correction', 'orange');
-% legend('without backing', 'with backing');
-% 
-% TTF = (1./(((M11+(M11.^2./Z2))./M12)-(M12./Z2))).*((Z.*B{2})./(B{3}.*(B{1}+Z)-(B{2}.^2)));
-% [TTF_modulo, TTF_fase] = conv_i(TTF);
-% TTF_b = (1./(((M11+(M11.^2./Z2))./M12)-(M12./Z2))).*((Z.*B_b{2})./(B_b{3}.*(B_b{1}+Z)-(B_b{2}.^2)));
-% [TTF_modulo_b, TTF_fase_b] = conv_i(TTF_b);
-% 
-% figure(6)
-% Grafico(f,TTF_modulo, TTF_fase,'TTF Backing with l-correction', 'blue');
-% hold on;
-% Grafico(f, TTF_modulo_b, TTF_fase_b, 'TTF Backing with l-correction', 'orange');
-% legend('without backing', 'with backing');
-% 
-% % Calcola il valore massimo dell'ampiezza
-% A_max = max(TTF_modulo);
-% A_max_b = max(TTF_modulo_b);
-% 
-% % Calcola il livello -3 dB
-% A_3dB = A_max -3;
-% A_6dB = A_max -6;
-% 
-% A_3dB_b = A_max_b -3;
-% A_6dB_b = A_max_b -6;
-% 
-% % Trova le frequenze a cui l'ampiezza è prossima a A_3dB
-% indices_3db = find(TTF_modulo >= A_3dB);
-% indices_6db = find(TTF_modulo >= A_6dB);
-% 
-% indices_3db_b = find(TTF_modulo_b >= A_3dB_b);
-% indices_6db_b = find(TTF_modulo_b >= A_6dB_b);
-% 
-% % Limite inferiore e superiore della banda a -3 dB
-% f_low_3dB = f(indices_3db(1));
-% f_high_3dB = f(indices_3db(end));
-% f_low_6dB = f(indices_6db(1));
-% f_high_6dB = f(indices_6db(end));
-% 
-% f_low_3dB_b = f(indices_3db_b(1));
-% f_high_3dB_b = f(indices_3db_b(end));
-% f_low_6dB_b = f(indices_6db_b(1));
-% f_high_6dB_b = f(indices_6db_b(end));
-% 
-% fc_3dB = (f_low_3dB + f_high_3dB)/2;
-% fc_6dB = (f_low_6dB + f_high_6dB)/2;
-% 
-% fc_3dB_b = (f_low_3dB_b + f_high_3dB_b)/2;
-% fc_6dB_b = (f_low_6dB_b + f_high_6dB_b)/2;
-% 
-% fractional_bandwidth_3dB = ((f_high_3dB - f_low_3dB)/fc_3dB)*100;
-% fractional_bandwidth_6dB = ((f_high_6dB - f_low_6dB)/fc_6dB)*100;
-% 
-% fractional_bandwidth_3dB_b = ((f_high_3dB_b - f_low_3dB_b)/fc_3dB_b)*100;
-% fractional_bandwidth_6dB_b = ((f_high_6dB_b - f_low_6dB_b)/fc_6dB_b)*100;
-% 
-% % Calcola la larghezza di banda
-% fprintf('Fractional bandwidth at -3dB without backing %0.3f%%\n', fractional_bandwidth_3dB);
-% fprintf('Fractional bandwidth at -6dB without backing %0.3f%%\n', fractional_bandwidth_6dB);
-% fprintf('Fractional bandwidth at -3dB with backing %0.3f%%\n', fractional_bandwidth_3dB_b);
-% fprintf('Fractional bandwidth at -6dB with backing %0.3f%%\n', fractional_bandwidth_6dB_b);
+%% Funzione di trasferimento in trasmissione della ceramica con backing e matching plate con spessore ottimizzato
+
+l_plate = l_plate_best;
+
+M = calcolaMatriceM(ZoP, k_plate, l_plate);
+Zeq = M{1,1} - ( (M{1,2} .^ 2) ./ (Z2 + M{1,1}) );
+[Zin_with_backing_with_plate, FTT_with_backing_pzt, ~] = calcolaFunzioniDiTrasferimento(B_with_backing, Zeq, Zel);
+FTT_with_backing_pzt = FTT_with_backing_pzt{1} .* exp(1j*deg2rad(FTT_with_backing_pzt{2}));
+FTT_plate = ( M{1,2} .* Z2 ) ./ ( M{1,1}.*Z2 + M{1,1}.^2 - M{1,2}.^2);
+FTT_with_backing_with_plate = FTT_with_backing_pzt .* FTT_plate;
+[moduloFTT_with_backing_with_plate, faseFTT_with_backing_with_plate] = calcolaModuloEFase(FTT_with_backing_with_plate, true, true);
+FTT_with_backing_with_plate = {moduloFTT_with_backing_with_plate, faseFTT_with_backing_with_plate};
+
+l_plate_scaled = l_plate * 1e+03;
+l_plate_scaled = round(l_plate_scaled, 2);
+legendTitle = strcat("Matching Plate made of E-Solder 3022 (", string(l_plate_scaled), " [mm])");
+figure(6)
+stampaGrafici(f, Zin_with_backing_with_plate{1}, Zin_with_backing_with_plate{2}, "Zin with Backing and Optimized Matching Plate", 'orange', "Zin", "Zin", " with backing with plate", legendTitle);
+figure(7)
+stampaGrafici(f, FTT_with_backing_with_plate{1}, FTT_with_backing_with_plate{2}, "TTF with Backing and Optimized Matching Plate", 'orange', "TTF", "TTF", " with backing with plate", legendTitle);
+
+A_max_with_backing_with_plate = max(moduloFTT_with_backing_with_plate);
+A_3dB_with_backing_with_plate = A_max_with_backing_with_plate - 3;
+A_6dB_with_backing_with_plate = A_max_with_backing_with_plate - 6;
+indices_3db_with_backing_with_plate = find(moduloFTT_with_backing_with_plate >= A_3dB_with_backing_with_plate);
+indices_6db_with_backing_with_plate = find(moduloFTT_with_backing_with_plate >= A_6dB_with_backing_with_plate);
+fl_3dB_with_backing_with_plate = f(indices_3db_with_backing_with_plate(1));
+fh_3dB_with_backing_with_plate = f(indices_3db_with_backing_with_plate(end));
+fl_6dB_with_backing_with_plate = f(indices_6db_with_backing_with_plate(1));
+fh_6dB_with_backing_with_plate = f(indices_6db_with_backing_with_plate(end));
+fc_3dB_with_backing_with_plate = (fl_3dB_with_backing_with_plate + fh_3dB_with_backing_with_plate)/2;
+fc_6dB_with_backing_with_plate = (fl_6dB_with_backing_with_plate + fh_6dB_with_backing_with_plate)/2;
+FBW_3dB_with_backing_with_plate = ( (fh_3dB_with_backing_with_plate - fl_3dB_with_backing_with_plate) / fc_3dB_with_backing_with_plate ) * 100;
+FBW_6dB_with_backing_with_plate = ( (fh_6dB_with_backing_with_plate - fl_6dB_with_backing_with_plate) / fc_6dB_with_backing_with_plate ) * 100;
+
+cprintf('Comments',"\nBanda Frazionaria a -3dB con backing e plate ottimizzato: FBW=%0.2f%%", string(FBW_3dB_with_backing_with_plate));
+cprintf('Comments',"\nBanda Frazionaria a -6dB con backing e plate ottimizzato: FBW=%0.2f%%", string(FBW_6dB_with_backing_with_plate));
+cprintf('Text',"\n");
