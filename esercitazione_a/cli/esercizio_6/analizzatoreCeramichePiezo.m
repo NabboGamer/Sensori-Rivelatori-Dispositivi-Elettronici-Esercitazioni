96b9ba95356f30d5b6d295ae2a2f709156dc9814
Michael Pio <michaelpio.stolfi@studenti.unibas.it>
2025-11-01 19:11:20 +0100

    αfeat(esercitazione_a/cli): calcolo parametri caratteristici analizzatore di ceramiche piezoelettriche

--- esercitazione_a/cli/esercizio_6/analizzatoreCeramichePiezo.m
+++ esercitazione_a/cli/esercizio_6/analizzatoreCeramichePiezo.m
@@ -6,6 +6,8 @@ addpath('../utility/');
 addpath('../../core/');
 evalin('base', 'clear'), close all; clc;
 
+%% Acquisizione parametri misurati della ceramica
+
 % Acquisizione di forma e dimensioni dell'elemento
 [areaFaccia, l] = geometryPicker();
 
@@ -26,5 +28,31 @@ evalin( 'base', 'clear("csv")' );
 figure(1);
 stampaGrafici(f, Zin{1}, Zin{2}, "Zin: input impedance", 'blue', "Zin", "Zin");
 
+%% Calcolo parametri caratteristici della ceramica a partire dai valori prelevati al passo precedente
 
+% Tutte le formule presenti in questa sezione sono state discusse e ricavate
+% nel pdf "Procedura di deduzione della tipologia di una ceramica piezoelettrica"
+
+% Calcolo rho(presupponendo volume cilindrico)
+rho = m / (areaFaccia * l);
+
+% Calcolo c33;
+[~, index_max] = max(Zin{1});
+fr = f(1,index_max);
+c33 = 4 * (fr^2) * (l^2) * rho;
+
+% Calcolo beta33
+beta33 = areaFaccia/(C0 * l);
+
+%% Stima della tipologia più probabile di ceramica partendo dai parametri calcolati al passo precedente
+strutturaProprietaPzt = caricaStrutturaProprietaPzt();
+
+% TODO: Per ogni riga della struttura precedente inserire una terza colonna
+% che tiene conto dello score totale. Ogni punto score viene assegnato
+% confrontando la singola proprietà calcolata(ad esempio rho) con tutte 
+% quelle degli elementi della struttura e viene assegnato alla pzt avente
+% la stessa proprietà in valore più vicino. Infine viene ordinata la
+% struttura in base a quella colonna in ordine decresecnte(la pzt con lo
+% score più alto sopra) e vengono stampati le prime 3 righe(solo colonna 1 
+% nome e colonna 3 score totale)
 
---
+++ esercitazione_a/core/caricaStrutturaProprietaPzt.m
@@ -0,0 +1,29 @@
+function strutturaProprietaPzt = caricaStrutturaProprietaPzt()
+    %CARICASTRUTTURAPROPRIETAPZT carica in memoria una struct MATLAB con proprietà utili delle ceramiche pzt attualmente supportate
+    
+    strutturaProprietaPzt={};
+
+    strutturaProprietaPzt{1,1} = "Pz21";
+    strutturaProprietaPzt{1,2} = [7780,  1.1100e+11, 5.7265e+07];
+
+    strutturaProprietaPzt{2,1} = "Pz23";
+    strutturaProprietaPzt{2,2} = [7700,  1.5400e+11, 1.2968e+08];
+
+    strutturaProprietaPzt{3,1} = "Pz24";
+    strutturaProprietaPzt{3,2} = [7700,  1.8100e+11, 4.7475e+08];
+
+    strutturaProprietaPzt{4,1} = "Pz26";
+    strutturaProprietaPzt{4,2} = [7700,  1.5800e+11, 1.6122e+08];
+
+    strutturaProprietaPzt{5,1} = "Pz27";
+    strutturaProprietaPzt{5,2} = [7700,  1.4400e+11, 123750000];
+
+    strutturaProprietaPzt{6,1} = "Pz28";
+    strutturaProprietaPzt{6,2} = [7700,  1.5200e+11, 2.2258e+08];
+
+    strutturaProprietaPzt{7,1} = "Pz29";
+    strutturaProprietaPzt{7,2} = [7460,  1.5100e+11, 9.2453e+07];
+
+    strutturaProprietaPzt{8,1} = "Pz34";
+    strutturaProprietaPzt{8,2} = [7550,  1.6700e+11, 4.7031e+03];
+end 
