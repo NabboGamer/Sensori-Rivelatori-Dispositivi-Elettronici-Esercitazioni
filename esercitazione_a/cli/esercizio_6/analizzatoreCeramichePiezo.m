2f9967effb7be3af8ff55a3c139e49d8641b9287
Michael Pio <michaelpio.stolfi@studenti.unibas.it>
2025-11-02 16:46:57 +0100

    αfeat(esercitazione_a/cli): disabilitato meccanismo di scoring discreto analizzatore di ceramiche piezoelettriche
    Il meccanismo di scoring discreto è stato disabilitato lasciando soltanto quello continuo.
    Questo poichè portava a risultati che ignoravano troppa informazione ovvero quanto effettivamente un materiale fosse vicino al valore misurato.
    Ora è rimasto solo il meccanismo di scoring continuo, con l'errore relativo medio, che favorisce quei materiali che in generale su tutte e tre le proprietà sono piuttosto vicini al valore misurato.

--- esercitazione_a/cli/esercizio_6/analizzatoreCeramichePiezo.m
+++ esercitazione_a/cli/esercizio_6/analizzatoreCeramichePiezo.m
@@ -48,45 +48,44 @@ proprietaMisurate = [rho, c33, beta33];
 
 %% Stima della tipologia più probabile di ceramica partendo dai parametri calcolati al passo precedente
 strutturaProprietaPzt = caricaStrutturaProprietaPzt();
-
-% Colonna 3: punteggio discreto "per proprietà" (nearest neighbor per proprietà)
-[strutturaProprietaPzt{:,3}] = deal(0);    % inizializza score a 0
 r = size(strutturaProprietaPzt,1);
 
-for i = 1:3
-    indiceMigliore = 0;
-    differenzaMigliore = inf;
-    for j = 1:r
-        proprietaIterazione = strutturaProprietaPzt{j,2}(i);
-        differenzaProprieta = abs(proprietaMisurate(i) - proprietaIterazione);
-        if differenzaProprieta < differenzaMigliore
-            differenzaMigliore = differenzaProprieta;
-            indiceMigliore = j;
-        end
-    end
-    strutturaProprietaPzt{indiceMigliore,3} = strutturaProprietaPzt{indiceMigliore,3} + 1;
-end
+% % Colonna 3: nearest neighbor per proprietà (ranking "discreto")
+% [strutturaProprietaPzt{:,3}] = deal(0);    % inizializza score a 0
+% 
+% for i = 1:3
+%     indiceMigliore = 0;
+%     differenzaMigliore = inf;
+%     for j = 1:r
+%         proprietaIterazione = strutturaProprietaPzt{j,2}(i);
+%         differenzaProprieta = abs(proprietaMisurate(i) - proprietaIterazione);
+%         if differenzaProprieta < differenzaMigliore
+%             differenzaMigliore = differenzaProprieta;
+%             indiceMigliore = j;
+%         end
+%     end
+%     strutturaProprietaPzt{indiceMigliore,3} = strutturaProprietaPzt{indiceMigliore,3} + 1;
+% end
 
 % Colonna 4: errore relativo medio sulle 3 proprietà (ranking "continuo")
 M = vertcat(strutturaProprietaPzt{:,2});                  % Nx3
 errRel = abs((M - proprietaMisurate)./proprietaMisurate); % Nx3
 errTot = mean(errRel,2);                                  % Nx1
-for j = 1:r
-    strutturaProprietaPzt{j,4} = errTot(j);
+for i = 1:r
+    strutturaProprietaPzt{i,4} = errTot(i);
 end
 
-% Ordina: prima per punteggio discreto (desc), poi per errore totale (asc)
-strutturaProprietaPzt = sortrows(strutturaProprietaPzt, [3 4], {'descend','ascend'});
+% Ordino le righe rispetto al loro risultato
+strutturaProprietaPzt = sortrows(strutturaProprietaPzt, [4], {'ascend'});
 
-% Mostra le prime 3: nome, score, errore medio
+% Mostra le prime 3: nome, errore medio
 topN = min(3, r);
 idx   = 1:topN;
 nomi  = vertcat(strutturaProprietaPzt{idx,1});      % string array
-score = cell2mat(strutturaProprietaPzt(idx,3));     % Nx1 double
+% score = cell2mat(strutturaProprietaPzt(idx,3));     % Nx1 double
 err   = cell2mat(strutturaProprietaPzt(idx,4));     % Nx1 double
 
-T = table(nomi, score, err, 'VariableNames', {'Materiale','Score','Errore Medio Relativo'});
-T.Properties.RowNames = cellstr(T.Materiale);
-T.Materiale = [];
-cprintf('Text', "\n");
+T = table(nomi, err, 'VariableNames', {'Materiali più probabili', 'Errore Relativo Medio'});
+T.("Materiali più probabili") = categorical(T.("Materiali più probabili"));
+disp(newline)
 disp(T)
