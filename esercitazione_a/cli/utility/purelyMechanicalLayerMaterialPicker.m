e9d36d7515c060e7db6fdb692ea9cbcf03eb9f0a
Michael Pio <michaelpio.stolfi@studenti.unibas.it>
2025-11-23 17:23:32 +0100

    αfix(esercitazione_a/cli): cambiata risorsa di riferimento per le proprietà acustiche dei materiali utilizzati per gli strati puramente meccanici
    La risorsa attuale ha molti più materiali e inoltre fornisce anche il Rapporto di Poisson, che è un parametro indispensabile per la simulazione in ANSYS.
    Utilizzando quindi questa nuova risorsa è possibile ottenere tutti i parametri utili per entrambe le esercitazioni senza creare disallineamenti.

--- esercitazione_a/cli/utility/purelyMechanicalLayerMaterialPicker.m
+++ esercitazione_a/cli/utility/purelyMechanicalLayerMaterialPicker.m
@@ -1,7 +1,7 @@
 function [rho, z, v] = purelyMechanicalLayerMaterialPicker(string)
     % PURELYMECHANICALLAYERMATERIALPICKER permette di acquisire parametri specifici relativi a una serie di materiali impiegati nella produzione di strati puramente meccanici
 
-    % I seguenti valori sono stati prelevati dal pdf "Velocity and Acoustic Impedance Table"
+    % I seguenti valori sono stati prelevati dal pdf "Acoustic Properties of Solids, Onda Corporation"
 
     % Come suggerito nel pdf "Anatomy of Multiple Element Transducers",
     % per fare in modo che l'energia generata dalla ceramica sia diretta
@@ -19,8 +19,8 @@ function [rho, z, v] = purelyMechanicalLayerMaterialPicker(string)
 
     cprintf('Text',"\n");
     cprintf('Text', "%s: \n", string);
-    cprintf('Text', '\t 1) Acciao inossidabile\n');
-    cprintf('Text', '\t 2) Alluminio \n');
+    cprintf('Text', '\t 1) Acciaio inossidabile 347 \n');
+    cprintf('Text', '\t 2) Alluminio 6262-T9 \n');
     cprintf('Text', '\t 3) Ferro \n');
     cprintf('Text', '\t 4) Titanio \n');
     cprintf('Text', '\t 5) Tungsteno \n');
@@ -29,28 +29,32 @@ function [rho, z, v] = purelyMechanicalLayerMaterialPicker(string)
     switch choice 
         
         case 1
-            v = 5.8e+03;  % m/s
-            z = 45.4e+06; % Rayl(Kg*s^-1*m^-2)
+            v   = 5.9e+03;  % m/s
+            rho = 7.89e+03; % Kg/m^3
+            z   = 45.7e+06; % Rayl(Kg*s^-1*m^-2)
 
         case 2
-            v = 6.3e+03; 
-            z = 17.0e+06;
+            v   = 6.38e+03;
+            rho = 2.73e+03;
+            z   = 17.41e+06;
         
         case 3
-            v = 5.9e+03;
-            z = 45.4e+06;
+            v   = 5.9e+03;
+            rho = 7.69e+03;
+            z   = 46.4e+06;
         
         case 4
-            v = 6.1e+03;
-            z = 27.3e+06;
+            v   = 6.1e+03;
+            rho = 4.48e+03;
+            z   = 27.3e+06;
             
         case 5
-            v = 5.2e+03;
-            z = 101.0e+06;
+            v   = 5.2e+03;
+            rho = 19.4e+03;
+            z   = 101.0e+06;
         
     end
     
-    rho = z/v;
 end
 
 
