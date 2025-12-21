0da2f680e48e3a62b43d468018517d72bf8ec610
Emanuele Pio Santangelo <emanuele@MacBookPro.homenet.telecomitalia.it>
2025-12-21 18:23:31 +0100

    #refactor(esercitazione_a/cli/utility): aggiunto valore di output 'fileName' a csvPicker;

--- esercitazione_a/cli/utility/csvPicker.m
+++ esercitazione_a/cli/utility/csvPicker.m
@@ -1,65 +1,65 @@
-function csv = csvPicker()
-    % CSVPICKER permette di scegliere tramite File Choser un file di tipo CSV
+function [csv, fileName] = csvPicker()
+% CSVPICKER permette di scegliere tramite File Choser un file di tipo CSV
 
-    % Specifico i formati di file desiderati
-    desiredFormats = {'*.csv'};
-    
-    % Apro il file choser
-    currentPath = pwd;
-    defaultPath = fullfile(currentPath, '..', '..', 'res');
-    defaultPath = char(java.io.File(defaultPath).getCanonicalPath());
-    [fileName, filePath] = uigetfile(desiredFormats, ...
-                                     'Seleziona un file CSV da caricare', ...
-                                     defaultPath);
-    
-    % Controllo se l'utente ha premuto "Annulla" o ha chiuso la finestra
-    if isequal(fileName, 0)
-        csv = -1;
-        cprintf('Errors', 'Nessun file selezionato! \n');
-        return;
-    end
-    
-    % Rimuovo gli asterischi dalla stringa del formato del file
-    formatsDesiredWithoutAsterixes = cellfun(@(x) strrep(x, '*', ''), ...
-                                             desiredFormats, ...
-                                             'UniformOutput', false);
+% Specifico i formati di file desiderati
+desiredFormats = {'*.csv'};
 
-    % Verifico se il file ha uno dei formati desiderati
-    validFormat = false;
-    for i = 1:length(formatsDesiredWithoutAsterixes)
-        if endsWith(lower(fileName), lower(formatsDesiredWithoutAsterixes{i}))
-            validFormat = true;
-            break;
-        end
-    end
-    
-    if ~validFormat
-        csv = -1;
-        cprintf('Errors', 'Formato del file non valido! \n');
-        return;
-    end
-        
-    cprintf('Text', '\n');
-    cprintf('Text', 'CSV selezionato correttamente! \n');
-    
-    completeFilePath = fullfile(filePath, fileName);
+% Apro il file choser
+currentPath = pwd;
+defaultPath = fullfile(currentPath, '..', '..', 'res');
+defaultPath = char(java.io.File(defaultPath).getCanonicalPath());
+[fileName, filePath] = uigetfile(desiredFormats, ...
+    'Seleziona un file CSV da caricare', ...
+    defaultPath);
 
-    cprintf('Text', 'Prego attendere mentre il CSV viene caricato in memoria... \n');
-    
-    oldWarnState = warning('query', 'all');
-    warning('off', 'all');
-    lastwarn('');
-    try
-        csv = csvLoader(completeFilePath);
-        [msg, ~] = lastwarn;
-        if ~isempty(msg)
-            cprintf('SystemCommands', 'Il file è in sola lettura! \n');
-            lastwarn('');
-        end
-        warning(oldWarnState);
-    catch
-        csv = -1;
+% Controllo se l'utente ha premuto "Annulla" o ha chiuso la finestra
+if isequal(fileName, 0)
+    csv = -1;
+    cprintf('Errors', 'Nessun file selezionato! \n');
+    return;
+end
+
+% Rimuovo gli asterischi dalla stringa del formato del file
+formatsDesiredWithoutAsterixes = cellfun(@(x) strrep(x, '*', ''), ...
+    desiredFormats, ...
+    'UniformOutput', false);
+
+% Verifico se il file ha uno dei formati desiderati
+validFormat = false;
+for i = 1:length(formatsDesiredWithoutAsterixes)
+    if endsWith(lower(fileName), lower(formatsDesiredWithoutAsterixes{i}))
+        validFormat = true;
+        break;
     end
-    cprintf('Text', 'CSV caricato correttamente in memoria! \n');
+end
+
+if ~validFormat
+    csv = -1;
+    cprintf('Errors', 'Formato del file non valido! \n');
+    return;
+end
+
+cprintf('Text', '\n');
+cprintf('Text', 'CSV selezionato correttamente! \n');
+
+completeFilePath = fullfile(filePath, fileName);
+
+cprintf('Text', 'Prego attendere mentre il CSV viene caricato in memoria... \n');
+
+oldWarnState = warning('query', 'all');
+warning('off', 'all');
+lastwarn('');
+try
+    csv = csvLoader(completeFilePath);
+    [msg, ~] = lastwarn;
+    if ~isempty(msg)
+        cprintf('SystemCommands', 'Il file è in sola lettura! \n');
+        lastwarn('');
+    end
+    warning(oldWarnState);
+catch
+    csv = -1;
+end
+cprintf('Text', 'CSV caricato correttamente in memoria! \n');
 
 end
